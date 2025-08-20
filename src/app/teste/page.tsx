0f6201b2ef4b92d735a541b93a10d47451b98439
DevPlayer02 <contato.giovanicf@gmail.com>
2025-08-19 21:36:29 -0300

    feat: add button and link components

---
+++ src/app/teste/page.tsx
@@ -0,0 +1,11 @@
+import Button from "../components/Button";
+import Link from "../components/Link";
+
+export default function Teste() {
+  return (
+    <section>
+          PAGINA TESTE
+          <Link href="/"> Ir para a Home </Link>
+    </section>
+  );
+}
