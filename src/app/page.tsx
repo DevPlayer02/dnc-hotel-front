8de82121f8e8de69e41f624f964f69a83ee65e93
DevPlayer02 <contato.giovanicf@gmail.com>
2025-08-19 22:54:50 -0300

    feat: add input image component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,17 @@
 import Button from "./components/Button";
 import Link from "./components/Link";
 import TextField from "./components/Form/TextField";
+import ImageField from "./components/Form/ImageField";
 
 export default function Home() {
   return (
     <section>
-      PAGINA PRINCIPAL
-      <Button>clique em mim</Button>
-      <Button appearance="secondary">clique em mim</Button>
+      PRINCIPAL PAGE
+      <Button>clique here</Button>
+      <Button appearance="secondary">clique here</Button>
       <Link href="teste"> Ir para teste </Link>
-      <TextField label="Nome completo" id="full_name" name="full_name" />
+      <TextField label="Full name" id="full_name" name="full_name" />
+      <ImageField label="Select the photo" id="profile_picture"/>
     </section>
   );
 }
