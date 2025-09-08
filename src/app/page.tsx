3a2d0536b8bee3b20db6755a1bf0d863dd8c2ebf
DevPlayer02 <contato.giovanicf@gmail.com>
2025-09-08 20:43:04 -0300

    feat: Add pagination component and feedback for user

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import Button from "./components/Button";
 import Link from "./components/Link";
 import TextField from "./components/Form/TextField";
 import ImageField from "./components/Form/ImageField";
+import Pagination from "./components/Pagination";
+import Alert from "./components/Alert";
 
 export default function Home() {
   return (
@@ -11,7 +13,9 @@ export default function Home() {
       <Button appearance="secondary">clique here</Button>
       <Link href="teste"> Ir para teste </Link>
       <TextField label="Full name" id="full_name" name="full_name" />
-      <ImageField label="Select the photo" id="profile_picture"/>
+      <ImageField name="profile_picture" label="Select the photo" id="profile_picture" />
+      <Pagination currentPage={7} destination="/" totalPages={100} />
+      <Alert type="success" >This is feedback for my user.</Alert>
     </section>
   );
 }
